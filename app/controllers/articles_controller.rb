c49c3aa9246de7bda11d644a16c2381897a7e571
srinivas-sateesh <srinivas.sateesh@gmail.com>
2016-04-21 18:09:42 +0000

    Add categories

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,6 @@
 class ArticlesController < ApplicationController
-  before_action :find_article, only: [:show]
-  before_action :authenticate_user!, except: [:index,:show]
+  before_action :find_article, only: [:show,:edit,:update, :destroy]
+  before_action :authenticate_user!, except: [:index, :show]
   def index
     if params[:category].blank?
       @articles = Article.all.order("created_at DESC")
@@ -27,6 +27,23 @@ class ArticlesController < ApplicationController
       end
   end
   
+  def edit
+  end
+ 
+  def update
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+ 
+  def destroy
+    @article.destroy
+     redirect_to root_path
+  end
+  
+    
   private 
   
   def find_article
